bb3daf715ac37672219d3b46ae5dcd46cb03ee8e
Omer <omer.viner@mail.huji.ac.il>
2018-05-04 23:59:09 +0300

    added plugins

---
+++ Assets/Scripts/Subdivide.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Torec;
+
+public class Subdivide : MonoBehaviour {
+	public Mesh mesh;
+	public int iter;
+	public CatmullClark.Options.BoundaryInterpolation m_boundaryInterpolation;
+	public Material m_material;
+
+	// Use this for initialization
+	void Start () {
+
+		mesh = CatmullClark.Subdivide(mesh, iter, new CatmullClark.Options {
+			boundaryInterpolation = m_boundaryInterpolation,
+		});
+
+		var obj = new GameObject("cell");
+		obj.transform.SetParent(this.transform);
+		obj.transform.position = new Vector3(0, 0, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/dnaCreatorScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dnaCreatorScript : MonoBehaviour {
+	public Transform brick;
+	public Transform dnaCreator;
+	public int dnaRotationSpeed;
+		
+	// Use this for initialization
+	void Start () {
+		for (int y = 0; y < 20; y++) {
+			var newNucleotide = Instantiate(brick, new Vector3(dnaCreator.position.x, y, dnaCreator.position.z), Quaternion.Euler(new Vector3(0, y * 10, 0)));
+			newNucleotide.transform.parent = dnaCreator.transform;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up * Time.deltaTime * dnaRotationSpeed, Space.World);
+
+	}
+}
